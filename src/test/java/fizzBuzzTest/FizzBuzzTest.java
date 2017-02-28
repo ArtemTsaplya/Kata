d366561df5533a036810d5e464d3499aa3b9088c
ArtemTsaplya <artem.tsaplya18@gmail.com>
2017-02-28 16:05:46 +0200

    FizzBuzz

--- src/test/java/fizzBuzzTest/FizzBuzzTest.java
+++ src/test/java/fizzBuzzTest/FizzBuzzTest.java
@@ -10,13 +10,11 @@ import static org.junit.Assert.assertThat;
  * Created by Artem on 28.02.2017.
  */
 public class FizzBuzzTest {
-    @Test
-    public void multiplesOfThreeTest() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String expected = "Fizz";
-         fizzBuzz.multiplesOfThree(27);
-        assertThat(actualResult, is(expected));
-        assert
-
-    }
+//    @Test
+//    public void multiplesOfThreeTest() throws Exception {
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+//        String expected = "Fizz";
+//         fizzBuzz.multiplesOfThree(27);
+//        assertThat(actualResult, is(expected));
+//    }
 }
